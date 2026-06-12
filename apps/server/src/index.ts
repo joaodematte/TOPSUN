163da870450051bf30977dec204cf4cd3239150e
João Dematte <jgdematte@gmail.com>
2026-06-12 08:41:27 -0300

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,34 @@
+import { trpcServer } from "@hono/trpc-server";
+import { createContext } from "@topsun/api/context";
+import { appRouter } from "@topsun/api/routers/index";
+import { env } from "@topsun/env/server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: env.CORS_ORIGIN,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+  }),
+);
+
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: (_opts, context) => {
+      return createContext({ context });
+    },
+  }),
+);
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+export default app;
